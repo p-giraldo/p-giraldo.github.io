6d82dd873377b104081f99e7bd899e84ee394e16
Paula Giraldo <paulagj2000@gmail.com>
2023-02-19 18:43:01 -0300

    cambio de color, fuente, alineación

---
+++ JS/for.js
@@ -0,0 +1,7 @@
+
+// for inicializacion; condicion; incremento/decremento
+var x = 10;
+
+for (x; x > 0; x--) {
+console.log("el valor de x es :"+x); //sentencia
+}
---
+++ JS/tipo-de-variable.js
@@ -0,0 +1,3 @@
+let x;       // Now x is undefined
+número = 5;       // Now x is a Number
+nombre = "John";  // Now x is a String
